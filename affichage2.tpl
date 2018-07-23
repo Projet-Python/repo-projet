c5630190956cadfc9b428e9d4bdd97f34cdb28e7
Samy Immoula <immoulasamy@gmail.com>
2018-07-23 22:57:34 +0200

    affichage 5 jours

--- affichage2.tpl
+++ affichage2.tpl
@@ -11,24 +11,39 @@
                 <header>
                 <h1>Application meteo</h1>
                 </header>
-		<div>	
-			<p> Date:{{city_weather['date']}}</p>			
-			<p> Temperature Minimle :{{city_weather['temperaturemin']}}</p>
-			<p> Temperature Maximale :{{city_weather['temperaturemax']}}</p>
-			<p> Description : {{city_weather['description']}}</p>
-		</div>
-		<div>
-                        <p> Date:{{city_weather2['date']}}</p>
-                        <p> Temperature Miniale :{{city_weather2['temperaturemin']}}</p>
-                        <p> Temperature Maximale :{{city_weather2['temperaturemax']}}</p>
-                        <p> Description : {{city_weather2['description']}}</p>
-                </div>
-		<div>
-                        <p> Date:{{city_weather3['date']}}</p>
-                        <p> Temperature Minimle :{{city_weather3['temperaturemin']}}</p>
-                        <p> Temperature Maximale :{{city_weather3['temperaturemax']}}</p>
-                        <p> Description : {{city_weather3['description']}}</p>
-                </div>
+
+			<div>	
+				<p> Date:{{city_weather['date']}}</p>			
+				<p> Temperature Minimle :{{city_weather['temperaturemin']}}</p>
+				<p> Temperature Maximale :{{city_weather['temperaturemax']}}</p>
+				<p> Description : {{city_weather['description']}}</p>
+			</div>
+			<div>
+                	        <p> Date:{{city_weather2['date']}}</p>
+                	        <p> Temperature Miniale :{{city_weather2['temperaturemin']}}</p>
+                	        <p> Temperature Maximale :{{city_weather2['temperaturemax']}}</p>
+                	        <p> Description : {{city_weather2['description']}}</p>
+              		</div>
+			<div>
+                        	<p> Date:{{city_weather3['date']}}</p>
+                        	<p> Temperature Minimle :{{city_weather3['temperaturemin']}}</p>
+                        	<p> Temperature Maximale :{{city_weather3['temperaturemax']}}</p>
+                        	<p> Description : {{city_weather3['description']}}</p>
+              	  	</div>
+			<div>
+                        	<p> Date:{{city_weather4['date']}}</p>
+                        	<p> Temperature Minimle :{{city_weather4['temperaturemin']}}</p>
+                        	<p> Temperature Maximale :{{city_weather4['temperaturemax']}}</p>
+                        	<p> Description : {{city_weather4['description']}}</p>
+               	 	</div>
+			<div>
+                        	<p> Date:{{city_weather5['date']}}</p>
+                        	<p> Temperature Minimle :{{city_weather5['temperaturemin']}}</p>
+                        	<p> Temperature Maximale :{{city_weather5['temperaturemax']}}</p>
+                        	<p> Description : {{city_weather5['description']}}</p>
+                	</div>
+		
+
 </body>
 </html>
 <style>
@@ -49,12 +64,13 @@ margin:0px;
 background-color:rgb(0,232,255);
 }
 div{
-width:400px;
+width:200px;
 margin: auto;
 margin-top:20px;
 text-align:center;
 border: 5px solid;
 border-color:rgb(0,232,255);
-
+display:inline-block;
 }
+
 </style>
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import Bottle, run, template, view, route, request,get,static_file
+from bottle import Bottle, run, template, view, route, request,get
 app =  Bottle()
 import requests
 @app.route('/search')
@@ -15,7 +15,7 @@ def result():
          city = request.forms.city
          url = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID=7582d9d4adc74a3c33d94751f5ff5d84&units=metric"  
          r = requests.get(url.format(city)).json()
-         city_weather = {
+         city_weather1 = {
                  'date':r['list'][0]['dt_txt'],
                  'temperaturemin':r['list'][0]['main']['temp_min'],
                  'temperaturemax':r['list'][0]['main']['temp_max'],
@@ -23,18 +23,32 @@ def result():
                  }
          
          city_weather2 = {
-                 'date':r['list'][1]['dt_txt'],
-                 'temperaturemin':r['list'][1]['main']['temp_min'],
-                 'temperaturemax':r['list'][1]['main']['temp_max'],
-                 'description':r['list'][1]['weather'][0]['description']
+                 'date':r['list'][8]['dt_txt'],
+                 'temperaturemin':r['list'][8]['main']['temp_min'],
+                 'temperaturemax':r['list'][8]['main']['temp_max'],
+                 'description':r['list'][8]['weather'][0]['description']
                  }
          city_weather3 = {
-                 'date':r['list'][2]['dt_txt'],
-                 'temperaturemin':r['list'][2]['main']['temp_min'],
-                 'temperaturemax':r['list'][2]['main']['temp_max'],
-                 'description':r['list'][2]['weather'][0]['description']
+                 'date':r['list'][16]['dt_txt'],
+                 'temperaturemin':r['list'][16]['main']['temp_min'],
+                 'temperaturemax':r['list'][16]['main']['temp_max'],
+                 'description':r['list'][16]['weather'][0]['description']
                  }
-         context = {'city_weather' : city_weather, 'city_weather2' : city_weather2, 'city_weather3' : city_weather3 }
+         city_weather4 = {
+                 'date':r['list'][24]['dt_txt'],
+                 'temperaturemin':r['list'][24]['main']['temp_min'],
+                 'temperaturemax':r['list'][24]['main']['temp_max'],
+                 'description':r['list'][24]['weather'][0]['description']
+                 }
+         city_weather5 = {
+                 'date':r['list'][32]['dt_txt'],
+                 'temperaturemin':r['list'][32]['main']['temp_min'],
+                 'temperaturemax':r['list'][32]['main']['temp_max'],
+                 'description':r['list'][32]['weather'][0]['description']
+                 }     
+         
+         context = {'city_weather' : city_weather1, 'city_weather2' : city_weather2, 'city_weather3' : city_weather3,'city_weather4' : city_weather4,
+                 'city_weather5' : city_weather5}
          return template('affichage2',context)
 run(app, host='localhost', port=5000, reloader=True)	
                                                             
