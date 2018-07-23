72ae8f7352548b2d9207910bea51463a7e684f12
Samy Immoula <immoulasamy@gmail.com>
2018-07-23 21:27:06 +0200

    Lien entre Api et form

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import Bottle, run, view, route, request,get,static_file#,requestapp =  Bottle()
+from bottle import Bottle, run, template, view, route, request,get,static_file
 app =  Bottle()
 import requests
 @app.route('/search')
 def search():
-	contain = {'title': "Saisir la ville pour obtenir ses prévision méteo :"}
-	return '''
-		Saisir la ville pour obtenir ses prévision méteo :
-                <form action="/result" method="post">
-                        Ville: <input name="city" type="text" />
-                        <input value="Rechercher" type="submit" />
-                </form>
-        '''
-
+	return template('affichage')
+        
 
 @app.route('/result', method='post')
 def result():
 #        context = {'title': "Prevision de la semaine. Ville :"}
          city = request.forms.city
-         url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=7582d9d4adc74a3c33d94751f5ff5d84&units=metric"  
+         url = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID=7582d9d4adc74a3c33d94751f5ff5d84&units=metric"  
          r = requests.get(url.format(city)).json()
          city_weather = {
-            'city' : city,
-            'Minimal temperature': r['main']['temp_min'],
-            'Maximal temperature': r['main']['temp_max'],
-            }
-         context = {'city_weather' : city_weather}
-         return (context)
-	
-run(app, host='localhost', port=5000, reloader=True)
+                 'date':r['list'][0]['dt_txt'],
+                 'temperaturemin':r['list'][0]['main']['temp_min'],
+                 'temperaturemax':r['list'][0]['main']['temp_max'],
+                 'description':r['list'][0]['weather'][0]['description']
+                 }
+         
+         city_weather2 = {
+                 'date':r['list'][1]['dt_txt'],
+                 'temperaturemin':r['list'][1]['main']['temp_min'],
+                 'temperaturemax':r['list'][1]['main']['temp_max'],
+                 'description':r['list'][1]['weather'][0]['description']
+                 }
+         city_weather3 = {
+                 'date':r['list'][2]['dt_txt'],
+                 'temperaturemin':r['list'][2]['main']['temp_min'],
+                 'temperaturemax':r['list'][2]['main']['temp_max'],
+                 'description':r['list'][2]['weather'][0]['description']
+                 }
+         context = {'city_weather' : city_weather, 'city_weather2' : city_weather2, 'city_weather3' : city_weather3 }
+         return template('affichage2',context)
+run(app, host='localhost', port=5000, reloader=True)	
                                                             
 
